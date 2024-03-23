a5ecc04dbbba964afb2b39443b815dd6a98fcca0
abanoub-ikb <abanoubikb@gmail.com>
2024-03-23 16:54:18 +0200

    contact form added

--- index.js
+++ index.js
@@ -1,66 +1,81 @@
-
+const BASE_URL = "https://penates-fe.vercel.app/";
+const scrollTopBtn = document.getElementById("scroll-top");
 const form = document.forms[0];
-const wideScreenTypeSelect = document.getElementById('type');
-const mobileScreenTypeSelect = document.querySelector('select[name=type]');
-const wideScreenLocationSelect = document.getElementById('location');
-const mobileScreenLocationSelect = document.querySelector('select[name=location]');
-const wideScreenBudgetSelect = document.getElementById('budget');
-const mobileScreenBudgetSelect = document.querySelector('select[name=budget]');
+const contactForm = document.forms[1];
+const mobileScreenTypeSelect = document.querySelector("select[name=type]");
+const mobileScreenLocationSelect = document.querySelector("select[name=location]");
+const mobileScreenBudgetSelect = document.querySelector("select[name=budget]");
+const phoneInputField = document.querySelector("#phone");
+const phoneErro  = document.querySelector(".alert");
 
-const BASE_URL = 'https://penates-fe.vercel.app/';
-let isWideScreen = true;
 
-form.addEventListener('submit',e=>{
-    e.preventDefault();
-    let dataIndex = isWideScreen ? 0 : 1;
-    const formData = new FormData(form);
-    let propertyType = formData.getAll('type')[dataIndex];
-    let location = formData.getAll('location')[dataIndex];
-    let budget = formData.getAll('budget')[dataIndex];
-    let targetUrl = `${BASE_URL}?type=${propertyType}&location=${location}&budget=${budget}`
-    // window.location.href = targetUrl
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript:
+    "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+  preferredCountries: ["eg", "ae", "kw", "sa", "us"],
 });
 
-form.addEventListener('click',e =>{
-    if(e.target.classList.contains('mobile') || e.target.classList.contains('wide')){
-        isWideScreen = e.target.classList.contains('mobile') ? false : true;
-    };
+window.onscroll = (e) => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.onclick = (e) => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(form);
+  let propertyType = formData.get("type");
+  let location = formData.get("location");
+  let budget = formData.get("budget");
+  let targetUrl = `${BASE_URL}?type=${propertyType}&location=${location}&budget=${budget}`;
+  console.log(targetUrl);
+  // window.location.href = targetUrl
 });
 
-function generateWideScreenSelectOptions(optionList = [],targetElement = HTMLDivElement,name = '',id = ''){
-    const frag = document.createDocumentFragment();
-    optionList.forEach((el,i)=>{
-        let inputId = `${id}-${i+1}`;
-        let input = document.createElement('input');
-        let inputLabel = document.createElement('label');
-        input.type = 'radio';
-        input.classList.add('selectopt');
-        input.value = el;
-        input.name = name
-        input.id = inputId;
-        inputLabel.classList.add('option');
-        inputLabel.setAttribute('for',inputId);
-        inputLabel.innerText = el;
-        frag.append(input,inputLabel);
-    });
-    targetElement.append(frag)
+
+contactForm.onsubmit = (e) => {
+  e.preventDefault();
+  if(phoneInput.isValidNumber()){
+    const formData = new FormData(contactForm);
+    formData.set("phone", phoneInput.getNumber());
+    contactForm.reset();
+    phoneErro.style.display = 'none';
+  }else{
+    phoneErro.style.display = 'block';
+  }
+  
 };
 
-function generateMobileScreenSelectOptions(optionList = [],targetElement = HTMLSelectElement){
-    const frag = document.createDocumentFragment();
-    optionList.forEach((el,i)=>{
-        let option = document.createElement('option');
-        option.value = el;
-        option.innerText = el;
-        frag.append(option);
-    });
-    targetElement.append(frag)
-};
+function generateMobileScreenSelectOptions(
+  optionList = [],
+  targetElement = HTMLSelectElement
+) {
+  const frag = document.createDocumentFragment();
+  optionList.forEach((el, i) => {
+    let option = document.createElement("option");
+    option.value = el;
+    option.innerText = el;
+    frag.append(option);
+  });
+  targetElement.append(frag);
+}
 
-generateWideScreenSelectOptions(['villa','house','apartment'],wideScreenTypeSelect,'type','type');
-generateWideScreenSelectOptions(['villa','house','apartment'],wideScreenLocationSelect,'location','location');
-generateWideScreenSelectOptions(['villa','house','apartment'],wideScreenBudgetSelect,'budget','budget');
-
-generateMobileScreenSelectOptions(['villa','house','apartment'],mobileScreenTypeSelect);
-generateMobileScreenSelectOptions(['villa','house','apartment'],mobileScreenLocationSelect);
-generateMobileScreenSelectOptions(['villa','house','apartment'],mobileScreenBudgetSelect);
+generateMobileScreenSelectOptions(
+  ["villa", "house", "apartment"],
+  mobileScreenTypeSelect
+);
+generateMobileScreenSelectOptions(
+  ["villa", "house", "apartment"],
+  mobileScreenLocationSelect
+);
+generateMobileScreenSelectOptions(
+  ["villa", "house", "apartment"],
+  mobileScreenBudgetSelect
+);
